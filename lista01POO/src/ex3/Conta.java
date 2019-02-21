705000af0a9c7a9544ee2afc0cec918d3b0556d6
Anoniimoo <tuliohorta15@gmail.com>
2019-02-21 14:29:03 -0300

    lista1 pronta

---
+++ lista01POO/src/ex3/Conta.java
@@ -0,0 +1,59 @@
+package ex3;
+
+public class Conta {
+    private int numConta, saldo;
+    private static int id=0;
+
+    public Conta(int numConta, int saldo) {
+        this.numConta = numConta;
+        this.saldo = saldo;
+        id++;
+    }
+
+    public Conta(int numConta){
+        this.numConta = numConta;
+        this.saldo = 0;
+        id++;
+    }
+
+    public Conta(){
+        this.numConta = 0;
+        this.saldo = 0;
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(int quantia){
+        this.saldo = this.saldo + quantia;
+    }
+
+    public boolean sacar(int quantia){
+        boolean possuiSaldoSuficiente;
+        if (this.saldo - quantia < 0){
+            possuiSaldoSuficiente = false;
+        }else{
+            this.saldo = this.saldo - quantia;
+            possuiSaldoSuficiente = true;
+        }
+        return possuiSaldoSuficiente;
+    }
+}
---
+++ lista01POO/src/ex4/Main.java
@@ -0,0 +1,9 @@
+package ex4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Conversor c = new Conversor();
+        System.out.println("A conversão de 10 pes para metros é: " + c.converterPesParaMetros(10));
+    }
+}
